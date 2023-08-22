dd75524915590d12dca5d6afffd8f2e708b82239
Sharon Arana <xel.dev3@inxel.mx>
2023-08-22 17:43:30 -0600

    Add append, prepend and insert methods for SLL

--- src/Estructuras_Datos/03-linkedList.js
+++ src/Estructuras_Datos/03-linkedList.js
@@ -13,7 +13,7 @@ Métodos:
 
 // 1 --> 2 --> 3 --> 4 --> null
 
-let singlyLinkedList = {
+/* let singlyLinkedList = {
   head: {
     value: 1,
     next: {
@@ -27,7 +27,7 @@ let singlyLinkedList = {
       },
     },
   },
-};
+}; */
 
 class Node {
   constructor(value) {
@@ -43,6 +43,49 @@ class MySinglyLinkedList {
     };
     this.tail = this.head;
     this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    return this;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    return this;
+  }
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(value) {
+    //reto
   }
 }
 
