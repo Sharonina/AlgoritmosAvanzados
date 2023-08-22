628ef8a50e348a6c2fc3593b2b33696fcc4f22f1
Sharon Arana <xel.dev3@inxel.mx>
2023-08-22 11:20:27 -0600

    Hash table implementation

--- src/Estructuras_Datos/02-hashtable.js
+++ src/Estructuras_Datos/02-hashtable.js
@@ -1,6 +1,7 @@
 /*
 Son similares a los objetos, ya que tienen un key y value, su diferencia es que tienen un paso extra,
 es decir una 'hash function' que viene siendo un address.
+Nosotros podemos indicar de que tamaño será el hashtable
 
 Métodos:
   -Insert
@@ -9,3 +10,28 @@ Métodos:
 
 Puede llegar a haber 'Colisiones' y esto es inevitable. Es decir que haya 2 elementos en un mismo bucket con el mismo address
 */
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+  hashMethod(key) {
+    let hash = 0;
+    for (let i = 0; (i = key.length); i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+  set(key, value) {
+    const address = this.hashMethod(key);
+
+    if (!this.data[address]) {
+      this.data[address] = [];
+    } else {
+      this.data[address].push([key, value]); //genera la colision
+      return this.data;
+    }
+  }
+}
+
+const myHashTable = new HashTable(50);
