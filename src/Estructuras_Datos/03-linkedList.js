11b36f8c37ea21a4f8c070e4f61ee23c50acd102
Sharon Arana <xel.dev3@inxel.mx>
2023-08-22 16:36:53 -0600

    Linked list - singly linked list description

---
+++ src/Estructuras_Datos/03-linkedList.js
@@ -0,0 +1,12 @@
+/*
+Son conjuntos de nodos ordenados que contienen valores.
+
+- Singly linked list: se compone del valor y el valor del 'next' (referencia)
+Los nodos estan ligados uno con el siguiente; No se puede regresar, se debe iniciar el proceso
+Métodos:
+  -Prepend (Agregar nodo al inicio)
+  -Append (Agregar nodo al final)
+  -Lookup / search
+  -Insert
+  -Delete
+*/
