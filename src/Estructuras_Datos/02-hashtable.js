c23fa43992923d1208404ccd2973662edc42f8da
Sharon Arana <xel.dev3@inxel.mx>
2023-08-22 10:10:18 -0600

    Hash table description

---
+++ src/Estructuras_Datos/02-hashtable.js
@@ -0,0 +1,11 @@
+/*
+Son similares a los objetos, ya que tienen un key y value, su diferencia es que tienen un paso extra,
+es decir una 'hash function' que viene siendo un address.
+
+Métodos:
+  -Insert
+  -Search
+  -Delete
+
+Puede llegar a haber 'Colisiones' y esto es inevitable. Es decir que haya 2 elementos en un mismo bucket con el mismo address
+*/
