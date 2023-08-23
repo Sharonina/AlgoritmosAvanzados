41079b17637a8391dfdd44a09171a7f23a1073d6
Sharon Arana <xel.dev3@inxel.mx>
2023-08-23 11:19:30 -0600

    Trees

---
+++ src/Estructuras_Datos/06-trees.js
@@ -0,0 +1,21 @@
+/*
+Ayudan a generar una ramificación de datos
+Se compone de root, parent, child, leaf, sibling, sub tree
+
+* Binary tree balanceado o perfecto se duplica con las ramificaciones
+* Binary search tree unbalanced se puede inclinar hacia un solo lado
+
+Binary search tree (buscar información): Sigue la regla de que los numeros que van decrementando van
+del lado izquierdo y los numeros que incrementan del lado derecho:
+
+        10
+      /   \
+    5      16
+  / \     / \
+2    6  12  24
+
+-Métodos:
+  -Search
+  -Insert
+  -Delete
+*/
