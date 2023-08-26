c94341fd04b58088cad0bcf4979171dedf44512d
Sharon Arana <xel.dev3@inxel.mx>
2023-08-26 07:07:11 -0600

    Add trees and insert method

--- src/Estructuras_Datos/06-trees.js
+++ src/Estructuras_Datos/06-trees.js
@@ -19,3 +19,43 @@ del lado izquierdo y los numeros que incrementan del lado derecho:
   -Insert
   -Delete
 */
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarysearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  //search(value) > el nodo completo
+}
+
+const tree = new BinarysearchTree();
